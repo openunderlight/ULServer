f9b4d97bfaba59fb9b8b399230347cf9f759d7cd
Disco Way <discoway@gmail.com>
2016-09-22 22:26:09 -0400

    Modified migration SQL.

--- migrations/migrate.201609.sql
+++ migrations/migrate.201609.sql
@@ -1,9 +1,8 @@
-ALTER TABLE player ADD COLUMN last_logout DATETIME NULL DEFAULT '1970-01-01 00:00:00';
-
 USE ul_player;
 --
 -- Table structure for table `pmare_lock`
 --
+ALTER TABLE player ADD COLUMN last_logout DATETIME NULL DEFAULT '1970-01-01 00:00:00';
 
 DROP TABLE IF EXISTS `pmare_lock`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -15,18 +14,6 @@ CREATE TABLE `pmare_lock` (
       `reason` text,
       `expired` tinyint(4) DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-08-25 17:28:25
 
 USE ul_level;
 
