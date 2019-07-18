bc86784d2fbaaa8dbc636b9904f0a5313aaa81eb
Christy Ganger <christyganger@gmail.com>
2019-07-17 18:41:03 -0700

    revert:modified version

--- build/scripts/uninstall.sh
+++ build/scripts/uninstall.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [ "$(id -u)" != "0" ]; then 
-	echo "Please run with sudo"
+if [ "$(id -u)" = "0" ]; then 
+	echo "Please don't run with sudo"
   exit 1
 fi
 DATABASES=(ul_billing ul_guild ul_item ul_level ul_player ul_server)
+
+echo -n "Please enter the database root password:"
+read -s ROOTPASS
 
 # Required directories
 INSTALLDIR=$HOME/lyra
@@ -13,7 +16,7 @@ rm -rf $INSTALLDIR
 
 for DATABASE in ${DATABASES[@]}
 do
-  mysql -e "DROP DATABASE $DATABASE"
+   mysql -u root -p"$ROOTPASS" -e "DROP DATABASE $DATABASE"
 done
 
 echo
