01638c034e209561ca2148f92899f4660fcc3f50
Christy Ganger <christyganger@gmail.com>
2019-07-17 19:44:49 -0700

    added remove of db users as well

--- build/scripts/uninstall.sh
+++ build/scripts/uninstall.sh
@@ -9,6 +9,11 @@ DATABASES=(ul_billing ul_guild ul_item ul_level ul_player ul_server)
 echo -n "Please enter the database root password:"
 read -s ROOTPASS
 
+echo "Available IP ADDRESSES: "`hostname -I`
+echo -n "it's possible it's localhost, or 127.0.0.1"
+echo -n "What is the ip to your database server: "
+read IPADDR
+
 # Required directories
 INSTALLDIR=$HOME/lyra
 
@@ -17,6 +22,7 @@ rm -rf $INSTALLDIR
 for DATABASE in ${DATABASES[@]}
 do
    mysql -u root -p"$ROOTPASS" -e "DROP DATABASE $DATABASE"
+   mysql -u root -p"$ROOTPASS" -e "DROP USER $DATABASE@$IPADDR";
 done
 
 echo
